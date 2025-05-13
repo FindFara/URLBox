a2931ed4fac5726aa73a71ca7d6cfeff4c97a3cc
Hosein Khakpour <h.khakpour@asax.ir>
2025-05-13 15:41:42 +0330

    add success page

---
+++ MyUrls/Controllers/DebitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyUrls.Controllers
+{
+    public class DebitController : Controller
+    {
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
